3207946f20f273cbbaf65b99432de57e044ae9b4
yarema velychko <100712051+avgsnusenjoyer@users.noreply.github.com>
2025-06-06 11:00:57 +0300

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,25 @@
+import networkx as nx
+from main import build_graph, compute_mst_and_stats
+
+def test_mst_properties():
+    edges = [
+        ('A', 'B', 4),
+        ('A', 'C', 3),
+        ('B', 'C', 1),
+        ('B', 'D', 2),
+        ('C', 'D', 4),
+        ('D', 'E', 2),
+        ('C', 'E', 5)
+    ]
+    G = build_graph(edges)
+    results = compute_mst_and_stats(G)
+    mst = results["mst"]
+    
+    assert len(mst.nodes) == len(G.nodes), "MST має містити всі вузли"
+    assert len(mst.edges) == len(G.nodes) - 1, "MST повинно мати n-1 ребро"
+    assert results["mst_length"] <= results["original_length"], "MST довше за оригінал"
+
+    print("MST проходить всі тести")
+
+if __name__ == "__main__":
+    test_mst_properties()
